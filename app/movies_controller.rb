598de9c3ef2980e792df4645e420cdfb75209ca4
Celina Jackson <celinanjackson@gmail.com>
2020-08-29 18:14:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,6 +23,12 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
+def can_be_created_in_a_block
+  Movie.create do |m|	  Movie.create do |m|
+    __	    m.title = "Woo!" 
+  end	  end
+end	end
+
 
 def can_get_the_first_item_in_the_database
   Movie.first.title 
